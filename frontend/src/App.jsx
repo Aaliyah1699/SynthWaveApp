c4acb9295cc77beddfc470a3cb96d55d0daa0506
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-13 12:56:00 -0600

    Setup layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <>
+            <Header />
+            <main className='py-3 bg-black text-white'>
+                <Container>
+                    <Outlet />
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
+};
 
-export default App
-
+export default App;
