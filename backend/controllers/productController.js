e5fc9b2690dfca9336f6f937aa668677827ec21d
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-13 22:09:06 -0600

    Create product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/ProductModel.js';
+
+// Fetch all products  // route -> GET /api/products // access => Public
+const getAllProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json({ products, count: products.length });
+});
+
+// Get single product by id  // route -> GET /api/products/:id // access => Public
+const getSingleProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        return res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+});
+
+export { getAllProducts, getSingleProduct };
