51a23ecf8b2ff1bd4011ab6a78649fcb2ea832b7
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-13 20:24:04 -0600

    Add and use product route

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 const port = process.env.PORT || 5000;
 
-connectDB() // Connect to MongoDB
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
+
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}...`));
