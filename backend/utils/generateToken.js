df730550ab26d8969fac3295e786eb680f793cc7
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-20 23:25:03 -0600

    Update expiration

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,15 +2,15 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
+        expiresIn: process.env.JWT_LIFETIME,
     });
-    // expiresIn: process.env.JWT_LIFETIME,
-    // expires: new Date(Date.now() + oneDay),//signed: true,
+
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        signed: true,
         sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        expires: 24 * 60 * 60 * 1000, // 1 day
     });
 };
 
