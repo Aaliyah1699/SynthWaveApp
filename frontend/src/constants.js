d7767ec2b0dfc7b316976d382bd1d3b0d58cfc76
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-21 17:50:09 -0600

    Removed comment

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,3 @@
-// export const BASE_URL =
-//     process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : '';
 
 export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
