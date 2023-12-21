0b115ba7369c98acdc10d22c8baf80e68de6a5f7
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-20 22:59:12 -0600

    Generate token

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+    // expiresIn: process.env.JWT_LIFETIME,
+    // expires: new Date(Date.now() + oneDay),//signed: true,
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+};
+
+export default generateToken;
