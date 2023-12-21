373b152847a252a0b5c27db976b7fe70980b5228
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-20 22:58:55 -0600

    Update middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,23 @@
 import jwt from 'jsonwebtoken';
-import asyncHandler from '../middleware/asyncHandler.js';
+import asyncHandler from './asyncHandler.js';
 import User from '../models/UserModel.js';
 
-// Protect routes
+// User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
     let token;
+
+    // Read JWT from the 'jwt' cookie
     token = req.cookies.jwt;
+
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
             req.user = await User.findById(decoded.userId).select('-password');
+
             next();
         } catch (error) {
-            console.log(error);
+            console.error(error);
             res.status(401);
             throw new Error('Not authorized, token failed');
         }
@@ -22,14 +27,13 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 });
 
-// Admin Middleware
+// User must be an admin
 const admin = (req, res, next) => {
     if (req.user && req.user.isAdmin) {
         next();
     } else {
         res.status(401);
-        throw new Error('Not authorized as admin');
+        throw new Error('Not authorized as an admin');
     }
 };
-
 export { protect, admin };
