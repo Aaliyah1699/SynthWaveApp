f0d4b14b2af04bc7317fdd31b51adbecc5fb9a49
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-24 17:27:45 -0600

    Add order routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 // Error Middleware
 app.use(notFound);
