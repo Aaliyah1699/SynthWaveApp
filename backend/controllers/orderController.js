228dfae23d5f434874735278cd6eb359cd538420
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-27 15:17:18 -0600

    Update order to paid

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,107 @@
+import { orderApiSlice } from '../../frontend/src/slices/ordersApiSlice.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Order from '../models/OrderModel.js';
+// import Product from '../models/ProductModel.js';
+// import { calcPrices } from '../utils/calcPrices.js';
+// import { verifyPayPalPayment, checkIfNewTransaction } from '../utils/paypal.js';
+
+// Create new order // route -> POST /api/orders // access => Private
+const addOrderItems = asyncHandler(async (req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+        const order = new Order({
+            // orderItems: dbOrderItems,
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined,
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
+});
+
+//  Get logged in user orders // @route ->  GET /api/orders/myorders // access => Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
+});
+
+// Get order by ID // route ->  GET /api/orders/:id // access => Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    );
+
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+// Update order to paid // route  -> PUT /api/orders/:id/pay // access => Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        // From paypal
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_Address: req.body.payer.email_address,
+        };
+        // Save in db
+        const updateOrder = await order.save();
+        res.status(200).json(updateOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+// Update order to delivered // route ->  PUT /api/orders/:id/deliver // access => Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    res.send('updateOrderToDelivered');
+});
+
+//  Get all orders // route  -> GET /api/orders // access => Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+    res.send('getOrders');
+});
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+};
