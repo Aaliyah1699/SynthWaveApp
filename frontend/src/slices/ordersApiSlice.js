fd8946a4e896b107abf09db2682cedebc025c6b9
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-27 15:38:45 -0600

    Pay order & client id

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,5 +1,5 @@
 import { apiSlice } from '../slices/apiSlice';
-import { ORDERS_URL } from '../constants';
+import { ORDERS_URL, PAYPAL_URL } from '../constants';
 
 export const orderApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -16,19 +16,19 @@ export const orderApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5,
         }),
-        // payOrder: builder.mutation({
-        //     query: ({ orderId, details }) => ({
-        //         url: `${ORDERS_URL}/${orderId}/pay`,
-        //         method: 'PUT',
-        //         body: details,
-        //     }),
-        // }),
-        // getPaypalClientId: builder.query({
-        //     query: () => ({
-        //         url: PAYPAL_URL,
-        //     }),
-        //     keepUnusedDataFor: 5,
-        // }),
+        payOrder: builder.mutation({
+            query: ({ orderId, details }) => ({
+                url: `${ORDERS_URL}/${orderId}/pay`,
+                method: 'PUT',
+                body: { ...details }, //details,
+            }),
+        }),
+        getPaypalClientId: builder.query({
+            query: () => ({
+                url: PAYPAL_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
         // getMyOrders: builder.query({
         //     query: () => ({
         //         url: `${ORDERS_URL}/mine`,
