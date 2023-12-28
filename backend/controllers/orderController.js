e288f3cd86ddf2660894948ab3eca630d955f4c7
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-28 14:05:09 -0600

    Get orders controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -93,7 +93,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 
 //  Get all orders // route  -> GET /api/orders // access => Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-    res.send('getOrders');
+    const orders = await Order.find({}).populate('user', 'id name');
+    res.status(200).json(orders);
 });
 
 export {
