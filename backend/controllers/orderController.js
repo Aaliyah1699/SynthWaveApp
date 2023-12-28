9716e9d97594fa84bb106e16c76d2887bda33e04
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-28 17:13:26 -0600

    Update order to delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -88,7 +88,19 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 
 // Update order to delivered // route ->  PUT /api/orders/:id/deliver // access => Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-    res.send('updateOrderToDelivered');
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 //  Get all orders // route  -> GET /api/orders // access => Private/Admin
