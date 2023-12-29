a5d2ef50fdee7c20c5de0ae0e99a15b97edc3735
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-29 12:10:24 -0600

    Create product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,4 +19,42 @@ const getSingleProduct = asyncHandler(async (req, res) => {
     }
 });
 
-export { getAllProducts, getSingleProduct };
+// Create product // route -> POST /api/products // access => Private (Admin)
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
+
+// Update product // route -> PUT /api/products/:id // access => Private (Admin)
+const updateProduct = asyncHandler(async (req, res) => {});
+
+// Delete product // route -> DELETE /api/products/:id // access => Private (Admin)
+const deleteProduct = asyncHandler(async (req, res) => {});
+
+// Create new review // route -> POST /api/products/:id/reviews // access => Private
+const createProductReview = asyncHandler(async (req, res) => {});
+
+// Get top rated products // route -> GET /api/products/top // access => Public
+const getTopProducts = asyncHandler(async (req, res) => {});
+
+export {
+    getAllProducts,
+    getSingleProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    createProductReview,
+    getTopProducts,
+};
