795b71f93cd57d9ea1990be3b0f21d26ab207222
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-12-29 15:13:16 -0600

    Add uploads route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -5,6 +6,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 
@@ -29,11 +31,16 @@ app.use(cookieParser());
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 // Paypal route
 app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+// Make uploads folder static
+const __dirname = path.resolve(); // set dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Error Middleware
 app.use(notFound);
