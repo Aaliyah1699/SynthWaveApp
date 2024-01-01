4c4fdc90cc2e0767f26f7cc41be6c151492e3536
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-01-01 17:26:37 -0600

    Delete product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -39,30 +39,40 @@ const createProduct = asyncHandler(async (req, res) => {
 
 // Update product // route -> PUT /api/products/:id // access => Private (Admin)
 const updateProduct = asyncHandler(async (req, res) => {
-     const { name, price, description, image, brand, category, countInStock } =
-         req.body;
+    const { name, price, description, image, brand, category, countInStock } =
+        req.body;
 
-     const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
-     if (product) {
-         product.name = name;
-         product.price = price;
-         product.description = description;
-         product.image = image;
-         product.brand = brand;
-         product.category = category;
-         product.countInStock = countInStock;
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
 
-         const updatedProduct = await product.save();
-         res.json(updatedProduct);
-     } else {
-         res.status(404);
-         throw new Error('Product not found');
-     }
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
 });
 
 // Delete product // route -> DELETE /api/products/:id // access => Private (Admin)
-const deleteProduct = asyncHandler(async (req, res) => {});
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        await Product.deleteOne({ _id: product._id });
+        res.json({ message: 'Product removed' });
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
 
 // Create new review // route -> POST /api/products/:id/reviews // access => Private
 const createProductReview = asyncHandler(async (req, res) => {});
