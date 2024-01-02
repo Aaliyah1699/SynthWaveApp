6fdd9ebbcdb8cebecbd58d66218cca58418bed82
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-01-01 19:50:23 -0600

    User edit screen route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,6 +28,7 @@ import OrderListScreen from './screens/admin/OrderListScreen.jsx';
 import ProductListScreen from './screens/admin/ProductListScreen.jsx';
 import ProductEditScreen from './screens/admin/ProductEditScreen.jsx';
 import UserListScreen from './screens/admin/UserListScreen.jsx';
+import UserEditScreen from './screens/admin/UserEditScreen.jsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -57,6 +58,10 @@ const router = createBrowserRouter(
                     element={<ProductEditScreen />}
                 />
                 <Route path='/admin/userlist' element={<UserListScreen />} />
+                <Route
+                    path='/admin/user/:id/edit'
+                    element={<UserEditScreen />}
+                />
             </Route>
         </Route>
     )
