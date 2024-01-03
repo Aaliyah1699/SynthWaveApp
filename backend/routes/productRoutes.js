c4f2d15c1ff75681b1d5c43ae7db1c437567c1f0
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-01-02 22:43:29 -0600

    Paypal

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,14 +10,15 @@ import {
     getTopProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
+import checkObjectId from '../middleware/checkObjectId.js';
 
 router.route('/').get(getAllProducts).post(protect, admin, createProduct);
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 router.get('/top', getTopProducts);
 router
     .route('/:id')
-    .get(getSingleProduct)
-    .put(protect, admin, updateProduct)
-    .delete(protect, admin, deleteProduct);
+    .get(checkObjectId, getSingleProduct)
+    .put(protect, admin, checkObjectId, updateProduct)
+    .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
