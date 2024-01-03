eae86e94bcb14a616f5d13fbae77d3a2565995ef
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-01-02 22:43:12 -0600

    Object id middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -9,14 +9,9 @@ const errorHandler = (err, req, res, next) => {
 
     let message = err.message;
 
-    // Check for cast error (bad ObjectId in mongoose)
-    if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        message = `Resource not found`;
-        statusCode = 404;
-    }
     res.status(statusCode).json({
         message,
-        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     });
 };
 
