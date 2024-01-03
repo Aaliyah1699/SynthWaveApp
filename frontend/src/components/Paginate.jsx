249616d4387b0fc31a3b312da298faef7ab57494
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-01-02 19:44:06 -0600

    Add keyword for search

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,7 +1,7 @@
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin = false }) => {
+const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
     return (
         pages > 1 && (
             <Pagination>
@@ -10,7 +10,9 @@ const Paginate = ({ pages, page, isAdmin = false }) => {
                         key={p + 1}
                         to={
                             !isAdmin
-                                ? `/page/${p + 1}`
+                                ? keyword
+                                    ? `/search/${keyword}/page/${p + 1}`
+                                    : `/page/${p + 1}`
                                 : `/admin/productlist/${p + 1}`
                         }
                     >
